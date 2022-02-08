99f4367f8a42d954ec080f85701a1ba5b5316f33
103SABAH_BEYZANUR <103SABAH_BEYZANUR@DESKTOP-TUMHS1A>
2022-02-08 11:05:08 +0300

    web service nesnesi projeye eklendi.

---
+++ AspNetMVC_API/WebServiceStudent.asmx.cs
@@ -0,0 +1,73 @@
+﻿using AspNetMVC_API_BLL.Repository;
+using AspNetMVC_API_Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace AspNetMVC_API
+{
+    /// <summary>
+    /// Summary description for WebServiceStudent
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebServiceStudent : System.Web.Services.WebService
+    {
+        //Global Alan
+        StudentRepo myStudentRepo = new StudentRepo();
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+        [WebMethod]
+        public List<Student> GetAll()
+        {
+            try
+            {
+                List<Student> list = myStudentRepo.GetAll();
+                return list;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        [WebMethod]
+        public string Insert(string name, string surname)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(surname))
+                {
+                    throw new Exception("name surname alanlarına mutlaka veri girilmelidir!");
+                }
+                Student newStudent = new Student() 
+                { 
+                    Name=name,
+                    Surname=surname
+                };
+                int insertResult = myStudentRepo.Insert(newStudent);
+                if (insertResult>0)
+                {
+                    //Birinci Yöntem
+                    return "Kayıt başarılı olarak eklendi. id=" + newStudent.Id;
+                    //İkinci Yöntem
+                }
+                else
+                {
+                    throw new Exception("Kayıt ekranı işleminde beklenmedik bir hata oluştu! ");
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
